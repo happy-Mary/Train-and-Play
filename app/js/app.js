bba28108cd308186646aac619fa9c458ed06a1d6
happy-Mary <mariashklovska1991@gmail.com>
2017-03-04 03:58:51 +0300

    TRAIN-121 HTML layout
    Routes in app.js added and ng-view

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
-var tpApp = angular.module("tpApp", []);
+var tpApp = angular.module("tpApp", ["ngRoute"]);
+
+// SPA - pages
+tpApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "templates/pages/main.html" })
+        .when("/landing", { templateUrl: "templates/pages/landing.html" })
+        .when("/user", { templateUrl: "templates/pages/user.html" });
+});
