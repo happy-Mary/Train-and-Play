d3b29d5efc30f05092f8a415d3611771a8a74d1b
happy-Mary <mariashklovska1991@gmail.com>
2017-03-16 21:28:45 +0300

    Train-59
    fixed # in location ng-if

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -2,9 +2,9 @@ tpApp.controller("MainController", function($scope, $location) {
 
     $scope.getLoc = function() {
         $scope.location = window.location;
-        $scope.pathHash = $scope.location.hash;
+        $scope.pathHash = $scope.location.hash.substr(1);
         console.log($scope.pathHash);
-        if ($scope.pathHash === "#/main") {
+        if ($scope.pathHash === "/main") {
             return true;
         } else {
             return false;
