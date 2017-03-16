fd99beffee467fdc0dc345ecf74ec8438442d085
happy-Mary <mariashklovska1991@gmail.com>
2017-03-16 21:34:10 +0300

    Added gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@
     path = require('path'),
     gcmq = require('gulp-group-css-media-queries'),
     concatCss = require('gulp-concat-css'),
-    gulpif = require('gulp-if'), 
+    gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
@@ -12,14 +12,15 @@
     autoprefixer = require('gulp-autoprefixer'),
     useref = require('gulp-useref'),
     smartgrid = require('smart-grid'),
+    notify = require("gulp-notify"),
     wiredep = require('wiredep').stream;
 
-gulp.task ('default', ['connect', 'watch'])
+gulp.task('default', ['connect', 'watch'])
 
 //build
-gulp.task('html', function () {
+gulp.task('html', function() {
     //var assets = useref.assets();
-    return gulp.src ('app/*.html')
+    return gulp.src('app/*.html')
         .pipe(useref())
         //.pipe(assets)
         //.pipe(gulpif('*.js', uglify()))
@@ -31,53 +32,52 @@ gulp.task('html', function () {
 });
 
 //css
-gulp.task('css', function () {
-  return gulp.src('app/css/*.css')
-    .pipe(gcmq())
-    .pipe(concatCss("bundle.css")) //как назвать файл
-    .pipe(autoprefixer({
+gulp.task('css', function() {
+    return gulp.src('app/css/*.css')
+        .pipe(gcmq())
+        .pipe(concatCss("bundle.css")) //как назвать файл
+        .pipe(autoprefixer({
             browsers: ['last 2 versions', '> 1%', 'IE 10'],
             cascade: true
         }))
-    //.pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename('bundle.min.css'))
-    .pipe(gulp.dest('app/css')) 
-    .pipe(connect.reload()); 
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename('bundle.min.css'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(connect.reload())
+        .pipe(notify("DONE!"));
+
 });
 
 //livereload
 gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
 });
 
 //less
-gulp.task('less', function () {
-  return gulp.src('app/src/**/*.less') // где лежит лесс
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('app/css')) // куда складывать css
-}); 
+gulp.task('less', function() {
+    return gulp.src('app/src/**/*.less') // где лежит лесс
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('app/css')); // куда складывать css
+});
 
 //bower
-gulp.task('bower', function () {
-  gulp.src('./app/index.html')
-    .pipe(wiredep({
-      directory : "app/bower_components"
-    }))
-    .pipe(gulp.dest('./app'));
+gulp.task('bower', function() {
+    gulp.src('./app/index.html')
+        .pipe(wiredep({
+            directory: "app/bower_components"
+        }))
+        .pipe(gulp.dest('./app'));
 });
 
 //watch
-gulp.task('watch', function(){
+gulp.task('watch', function() {
     gulp.watch('app/src/**/*.less', ['less'])
-    // gulp.watch('app/css/*.css', ['css'])
-    gulp.watch('app/*.html', ['html'])
+        // gulp.watch('app/css/*.css', ['css'])
+    gulp.watch('app/*.html', ['html']);
     // gulp.watch('bower.json', ['bower'])
-}); 
-
-
-
+});
