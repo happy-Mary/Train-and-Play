bb5899a85f9f4480e3e5df24e546275bfd095369
happy-Mary <mariashklovska1991@gmail.com>
2017-04-21 05:57:39 +0300

    TRAIN-276 account awards
    styled, dynamic building template by award-directive and ng-repeat
    TODO: write Angular for hiding and showing blocks

---
+++ app/js/directives/awardDirective.js
@@ -0,0 +1,13 @@
+tpApp.directive("award", function() {
+    return {
+        restrict: "A",
+        link: function($scope, element, attrs) {
+            // showing and hiding trenings
+            var proc = angular.element(document.querySelectorAll(".award"));
+            console.log(proc.length);
+            for (var i = 3; i < proc.length; i++) {
+                proc[i]
+            }
+        }
+    }
+});
